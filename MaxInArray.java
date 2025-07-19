c567fc0f9e47838ea396263cd60d5b44fc82c08d
Bhakti Pawar <bhaktipwr26@gmail.com>
2025-07-19 22:05:15 +0530

    Program to find max element in the array.

--- MaxInArray.java
+++ MaxInArray.java
@@ -2,11 +2,11 @@ class Max {
     void maximum() {
         int[] numbers = {22, 14, 8, 35, 19};
         // assuming first number is max
-        int max = Integer.MIN_VALUE;  
+        int max = Integer.MIN_VALUE;  //constant value in Java that represents the smallest possible integer
 
         for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] > max) {
-                max = numbers[i]; //constant value in Java that represents the smallest possible integer
+                max = numbers[i]; 
             }
         }
 
